e5128c492ae85f53bab47589ecd08b408d37756e
dh00mk3tu <cyberhybird@hotmail.com>
2020-09-09 11:48:45 +0530

    Bounce Property+Friction+Gravity test on a ball object to simulate fall physics

--- ElementJS-Test/element.js
+++ ElementJS-Test/element.js
@@ -1,57 +1,69 @@
-console.log("The Script Has Been Loaded");
+window.onload = function() {
+	var canvas = document.getElementById("canvas"),
+		context = canvas.getContext("2d"),
+		width = canvas.width = window.innerWidth,
+		height = canvas.height = window.innerHeight;
 
-var elementBasic;
+	var points = [],
+		bounce = 1,
+		gravity = 0.7,
+		friction = 1;
 
-function initElement() {
-    document.getElementById('component-title').innerHTML = 'Gravity Test';
-    elementBasic = new component(30, 30, "#057DCD", 80, 75);
-    console.log("Basic Element Loaded")
-    elementArea.start();
-    
-}
+	points.push({
+		x: 100,
+		y: 100,
+		oldx: 95,
+		oldy: 95
+	});
 
-var elementArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
-        this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-        this.interval = setInterval(updateElementArea, 20);        
-    },
-    stop : function() {
-        clearInterval(this.interval);
-    },    
-    clear : function() {
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
-}
 
-function component(width, height, color, x, y, type) {
-    this.type = type;
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;    
-    this.speedX = 0;
-    this.speedY = 0;    
-    this.gravity = .2;
-    this.gravitySpeed = 0;
-    console.log(this.gravity,this.speedX, this.speedY, this.x, this.y, this.gravitySpeed);
-    this.update = function() {
-        ctx = elementArea.context;
-        ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-    this.newPos = function() {
-        this.gravitySpeed += this.gravity;
-        this.x += this.speedX;
-        this.y += this.speedY + this.gravitySpeed;        
-    }
-}
 
-function updateElementArea() {
-    elementArea.clear();
-    elementBasic.newPos();
-    elementBasic.update();
-}
+	update();
+
+	function update() {
+		updatePoints();
+		renderPoints();
+		requestAnimationFrame(update);
+	}
+
+	function updatePoints() {
+		for(var i = 0; i < points.length; i++) {
+			var p = points[i],
+				vx = (p.x - p.oldx) * friction,
+				vy = (p.y - p.oldy) * friction;
+
+			p.oldx = p.x;
+			p.oldy = p.y;
+			p.x += vx;
+			p.y += vy;
+			p.y += gravity;
+
+			if(p.x > width) {
+				p.x = width;
+				p.oldx = p.x + vx * bounce;
+			}
+			else if(p.x < 0) {
+				p.x = 0;
+				p.oldx = p.x + vx * bounce;
+			}
+			if(p.y > height) {
+				p.y = height;
+				p.oldy = p.y + vy * bounce;
+			}
+			else if(p.y < 0) {
+				p.y = 0;
+				p.oldy = p.y + vy * bounce;
+			}
+		}
+	}
+
+	function renderPoints() {
+		context.clearRect(2, 2, width, height);
+		for(var i = 0; i < points.length; i++) {
+			var p = points[i];
+			context.beginPath();
+			context.arc(p.x, p.y, 35, 0, Math.PI * 2);
+			context.fill();
+		}
+	}
+};
