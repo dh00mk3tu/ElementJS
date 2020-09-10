ff8f882ec8e9491e7117fa5ebbe5026a80d43349
dh00mk3tu <cyberhybird@hotmail.com>
2020-09-10 09:06:50 +0530

    Fixed Canvas size>800x550, Centered Canvas and color change

--- ElementJS-Test/element.js
+++ ElementJS-Test/element.js
@@ -1,6 +1,7 @@
 window.onload = function() {
 	var canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
+
 		width = canvas.width = window.innerWidth,
 		height = canvas.height = window.innerHeight;
 
