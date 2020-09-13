aebd1ea539b19296ce65c89cdc963524a58695cd
dh00mk3tu <cyberhybird@hotmail.com>
2020-09-13 10:48:46 +0530

    Random tests>ElementBase

--- ElementJS-Test/element-base.js
+++ ElementJS-Test/element-base.js
@@ -9,10 +9,20 @@ window.onload = function() {
     //context.fillRect(0, 0,  width, height);
 
     // Random Lines 
-    for(var i = 0; i < 100; i++){
-        context.beginPath();
-        context.moveTo(Math.random()*width, Math.random()*height);
-        context.lineTo(Math.random()*width, Math.random()*height);
-        context.stroke();
+    // for(var i = 0; i < 100; i++){
+    //     context.beginPath();
+    //     context.moveTo(Math.random()*width, Math.random()*height);
+    //     context.lineTo(Math.random()*width, Math.random()*height);
+    //     context.stroke();
+    //}
+
+    // Trigonometry Trials 
+    context.translate(0, height/2);
+    context.scale(1, -1);
+    for(var angle = 0; angle < Math.PI*2; angle += 0.01){
+        console.log(Math.sin(angle));
+        var x = angle * 305,
+            y = Math.sin(angle) * 360;
+        context.fillRect(x, y, 5 ,5);
     }
 };
