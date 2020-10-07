b058530879737e514109b0c438324c5daa335404
dh00mk3tu <cyberhybird@hotmail.com>
2020-10-07 22:05:52 +0530

    This commit is made by Auto-Git 9, Wednesday 07 October 2020 09:57:22 PM IST

--- JS/ElementParticle.js
+++ JS/ElementParticle.js
@@ -1,1 +1,19 @@
-console.log("ElementParticle Loaded - Callback")
+console.log("ElementParticle Loaded - Callback");
+
+var particle = {
+    position: null,
+    velocity: null,
+
+    create: function (x,y, speed, direction) {
+        var entity = Object.create(this);
+        entity.position = vector.create(x, y);
+        entity.velocity = vector.create(0,0);
+        entity.velocity.setLength(speed);
+        entity.velocity.setAngle(direction);
+        return entity;
+    },
+
+    update: function() {
+        this.position.addTo(this.velocity);
+    }
+};
