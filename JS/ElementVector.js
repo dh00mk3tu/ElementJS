bcea1e133c89e3a7bd981b4368b254a8818baa8e
dh00mk3tu <cyberhybird@hotmail.com>
2020-10-07 22:27:01 +0530

    This commit is made by Auto-Git 29, Wednesday 07 October 2020 09:57:22 PM IST

--- JS/ElementVector.js
+++ JS/ElementVector.js
@@ -1,79 +1,85 @@
 console.log("Element Vector Loaded - Callback")
 
 var vector = {
-    _x: 1,
-    _y: 0,
+	_x: 1,
+	_y: 0,
 
-    create: function(x, y){
-        var obj = Object.create(this);
-        obj.setX(x);
-        obj.setY(y);
-        return obj;
-    },
+	create: function(x, y) {
+		var obj = Object.create(this);
+		obj.setX(x);
+		obj.setY(y);
+		return obj;
+	},
 
-    setX: function(value) {
-        this._x = value;
-    },
-    setY: function(value) {
-        this._y = value;
-    },
-    getX: function() {
-        return this._x;
-    },
-    getY: function() {
-        return this._y;
-    },
+	setX: function(value) {
+		this._x = value;
+	},
 
-    setAngle: function(angle) {
-        var length = this.getLenth();
-        this._x = Math.cos(angle) * length;
-        this._y = Math.sin(angle) * length;
-    },
+	getX: function() {
+		return this._x;
+	},
 
-    getAngle: function() {
-        return Math.atan2(this._y, this._x);
-    },
+	setY: function(value) {
+		this._y = value;
+	},
 
-    setLength: function(length) {
-        var angle = this.getAngle();
-        this._x = Math.cos(angle) * length;
-        this._y = Math.sin(angle) * length;
-    },
+	getY: function() {
+		return this._y;
+	},
 
-    getLenth: function(){
-        return Math.sqrt(this._x*this._x + this._y*this._y);
-    },
+	setAngle: function(angle) {
+		var length = this.getLength();
+		this._x = Math.cos(angle) * length;
+		this._y = Math.sin(angle) * length;
+	},
 
-    
+	getAngle: function() {
+		return Math.atan2(this._y, this._x);
+	},
 
-    add: function(v2){
-        return vector.create(this._x + v2.getX(), this._y + v2.getY());
-    },
-    subtract: function(v2){
-        return vector.create(this._x - v2.getX(), this._y - v2.getY());
-    },
-    multiply: function(val){
-        return vector.create(this._x * val, this-_y * val);
-    },
-    divide: function(val){
-        return vector.create(this._x / val, this-_y / val);
-    },
+	setLength: function(length) {
+		var angle = this.getAngle();
+		this._x = Math.cos(angle) * length;
+		this._y = Math.sin(angle) * length;
+	},
 
-    addTo: function(v2) {
-        this.x += v2.getX();
-        this._y += v2.getY();
-    },
+	getLength: function() {
+		return Math.sqrt(this._x * this._x + this._y * this._y);
+	},
 
-    subtractFrom: function(v2) {
-        this.x -= v2.getX();
-        this._y -= v2.getY();
-    },
-    multiplyBy: function(val) {
-        this.x *= val;
-        this._y *= val;
-    },
-    divideBy: function(val) {
-        this.x /= val;
-        this._y /= val;
-    }
+	add: function(v2) {
+		return vector.create(this._x + v2.getX(), this._y + v2.getY());
+	},
+
+	subtract: function(v2) {
+		return vector.create(this._x - v2.getX(), this._y - v2.getY());
+	},
+
+	multiply: function(val) {
+		return vector.create(this._x * val, this._y * val);
+	},
+
+	divide: function(val) {
+		return vector.create(this._x / val, this._y / val);
+	},
+
+	addTo: function(v2) {
+		this._x += v2.getX();
+		this._y += v2.getY();
+	},
+
+	subtractFrom: function(v2) {
+		this._x -= v2.getX();
+		this._y -= v2.getY();
+	},
+
+	multiplyBy: function(val) {
+		this._x *= val;
+		this._y *= val;
+	},
+
+	divideBy: function(val) {
+		this._x /= val;
+		this._y /= val;
+	}
 };
