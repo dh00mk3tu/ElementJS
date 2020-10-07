8def56914f71467c7c0440a4e3ec945aada39556
dh00mk3tu <cyberhybird@hotmail.com>
2020-10-08 03:03:39 +0530

    This commit is made by Auto-Git 94, Thursday 08 October 2020 01:26:31 AM IST

--- JS/element.js
+++ JS/element.js
@@ -9,19 +9,19 @@ window.onload = function() {
 		// entity = particle.create(100, 100, 3, Math.PI/3);
 
 		particles = [];
-		// nParticles = 3;
+		nParticles = 3;
 
 	document.getElementById("generate").addEventListener("click", generate);
 
 		function generate() {
 			//console.log("inside generate function");
-			var nParticles = document.getElementById("nParticles").value;
+			var n = document.getElementById("nParticles").value;
 			console.log("nParticles", nParticles);
 			for(var i=0; i < nParticles; i += 1) {
 				// particle.create(x, y, speed, direction)
 					particles.push(particle.create(width/2, height/2, Math.random()*2+1, Math.random() * Math.PI * 2));
 			}
-			update(nParticles);
+			update();
 			// alert(input);
 		}
 
