6491bc5fe78bf4b9c83dbb81150b53f89c616624
dh00mk3tu <cyberhybird@hotmail.com>
2020-10-15 06:49:21 +0530

    This commit is made by Auto-Git 1, Thursday 15 October 2020 06:49:20 AM IST

--- JS/Element.js
+++ JS/Element.js
@@ -3,114 +3,140 @@ window.onload = function() {
 		context = canvas.getContext("2d"),
 		width = canvas.width = window.innerWidth,
 		height = canvas.height = window.innerHeight,
-		ship = particle.create(width / 2, height / 2, 0, 0),
-		thrust = vector.create(0, 0),
-		angle = 0,
-		turningLeft = false,
-		turningRight = false,
-		thrusting = false;
+		// ship = particle.create(width / 2, height / 2, 0, 0),
+		// thrust = vector.create(0, 0),
+		// angle = 0,
+		// turningLeft = false,
+		// turningRight = false,
+		// thrusting = false;
+
+		sun = particle.create(width/2, height/2, 0, 0),
+		planet = particle.create(width/2 + 200, height/2, 10, -Math.PI/2);
+
+	sun.mass = 20000;
 
 	update();
-
-
-	document.body.addEventListener("keydown", function(event) {
-		// console.log(event.keyCode);
-		switch(event.keyCode) {
-			case 38: // up
-				thrusting = true;
-				break;
-				
-			case 37: // left
-				turningLeft = true;
-				break;
-				
-			case 39: // right
-				turningRight = true;
-				break;
-
-			default:
-				break;
-				
-		}
-	});
-
-	document.body.addEventListener("keyup", function(event) {
-		// console.log(event.keyCode);
-		switch(event.keyCode) {
-			case 38: // up
-				thrusting = false;
-				break;
-				
-			case 37: // left
-				turningLeft = false;
-				break;
-				
-			case 39: // right
-				turningRight = false;
-				break;
-
-			default:
-				break;
-				
-		}
-	});
 
 	function update() {
 		context.clearRect(0, 0, width, height);
 
-		if(turningLeft) {
-			angle -= 0.05;
-		}
-		if(turningRight) {
-			angle += 0.05;
-		}
-
-		thrust.setAngle(angle);
-
-		if(thrusting) {
-			thrust.setLength(0.1);
-		}
-		else {
-			thrust.setLength(0);
-		}
-
-		// animation goes here
-		ship.accelerate(thrust);
-		ship.update();
-
-		context.save();
-		context.translate(ship.position.getX(), ship.position.getY());
-		context.rotate(angle);
+		planet.gravityTo(sun);
+		planet.update();
 
 		context.beginPath();
-		context.moveTo(20, 0);
-		context.lineTo(-20, -14);
-		context.lineTo(-20, 14);
-		context.lineTo(20, 0);
-		if(thrusting) {
-			context.moveTo(-20, 0);
-			context.lineTo(-32, 0);
-		}
-		context.stroke();
+		context.fillStyle = "Yellow";
+		context.arc(sun.position.getX(), sun.position.getY(), 40, 0, Math.PI*2, false);
+		context.fill();
 
-		context.restore(); 	
+		context.beginPath();
+		context.fillStyle = "Blue";
+		context.arc(planet.position.getX(), planet.position.getY(), 5, 0, Math.PI*2, false);
+		context.fill();
 
-		if(ship.position.getX() > width) {
-			ship.position.setX(0);
-		}
-		if(ship.position.getX() < 0) {
-			ship.position.setX(width);
-		}
-		if(ship.position.getY() > height) {
-			ship.position.setY(0);
-		}
-		if(ship.position.getY() < 0) {
-			ship.position.setY(height);
-		}
-	
 		requestAnimationFrame(update);
 	}
-};	
+};
+
+
+
+// 	document.body.addEventListener("keydown", function(event) {
+// 		// console.log(event.keyCode);
+// 		switch(event.keyCode) {
+// 			case 38: // up
+// 				thrusting = true;
+// 				break;
+				
+// 			case 37: // left
+// 				turningLeft = true;
+// 				break;
+				
+// 			case 39: // right
+// 				turningRight = true;
+// 				break;
+
+// 			default:
+// 				break;
+				
+// 		}
+// 	});
+
+// 	document.body.addEventListener("keyup", function(event) {
+// 		// console.log(event.keyCode);
+// 		switch(event.keyCode) {
+// 			case 38: // up
+// 				thrusting = false;
+// 				break;
+				
+// 			case 37: // left
+// 				turningLeft = false;
+// 				break;
+				
+// 			case 39: // right
+// 				turningRight = false;
+// 				break;
+
+// 			default:
+// 				break;
+				
+// 		}
+// 	});
+
+// 	function update() {
+// 		context.clearRect(0, 0, width, height);
+
+// 		if(turningLeft) {
+// 			angle -= 0.05;
+// 		}
+// 		if(turningRight) {
+// 			angle += 0.05;
+// 		}
+
+// 		thrust.setAngle(angle);
+
+// 		if(thrusting) {
+// 			thrust.setLength(0.1);
+// 		}
+// 		else {
+// 			thrust.setLength(0);
+// 		}
+
+// 		// animation goes here
+// 		ship.accelerate(thrust);
+// 		ship.update();
+
+// 		context.save();
+// 		context.translate(ship.position.getX(), ship.position.getY());
+// 		context.rotate(angle);
+
+// 		context.beginPath();
+// 		context.moveTo(20, 0);
+// 		context.lineTo(-20, -14);
+// 		context.lineTo(-20, 14);
+// 		context.lineTo(20, 0);
+// 		if(thrusting) {
+// 			context.moveTo(-20, 0);
+// 			context.lineTo(-32, 0);
+// 		}
+// 		context.stroke();
+
+// 		context.restore(); 	
+
+// 		if(ship.position.getX() > width) {
+// 			ship.position.setX(0);
+// 		}
+// 		if(ship.position.getX() < 0) {
+// 			ship.position.setX(width);
+// 		}
+// 		if(ship.position.getY() > height) {
+// 			ship.position.setY(0);
+// 		}
+// 		if(ship.position.getY() < 0) {
+// 			ship.position.setY(height);
+// 		}
+	
+// 		requestAnimationFrame(update);
+// 	}
+// };	
 
 
 
@@ -226,7 +252,7 @@ window.onload = function() {
 // 			// }	
 // 			requestAnimationFrame(update);
 // 		}
-// 	};
+// };
 
 
 
--- JS/ElementParticle.js
+++ JS/ElementParticle.js
@@ -3,7 +3,8 @@ console.log("ElementParticle Loaded - Callback");
 var particle = {
     position: null,
     velocity: null,
-    gravity: null,
+    // gravity: null,
+    mass: 1,
 
     create: function (x,y, speed, direction, grav) {
         //console.log(direction);
@@ -23,5 +24,26 @@ var particle = {
     update: function() {
         this.velocity.addTo(this.gravity);
         this.position.addTo(this.velocity); 
+    },
+
+    angleTo: function(p2) {
+        return Math.atan2(p2.position.getY() - this.position.getY(), p2.position.getX() - this.position.getX());
+    },
+
+    distanceTo: function(p2) {
+        var dx = p2.position.getX() - this.position.getX();
+            dy = p2.position.getY() - this.position.getY();
+
+        return Math.sqrt(dx * dx + dy * dy);
+    },
+
+    gravityTo: function(p2){
+        var grav = vector.create(0,0),
+            dist = this.distanceTo(p2);
+
+        grav.setLength(p2.mass / (dist * dist));
+        grav.setAngle(this.angleTo(p2));
+
+        this.velocity.addTo(grav);
     }
 };
