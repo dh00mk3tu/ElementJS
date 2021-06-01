802e1219feeab70e0111324a21b227a833cf1cbd
dh00mk3tu <cyberhybird@hotmail.com>
2021-06-01 19:27:36 +0530

    This commit is made by Auto-Git 11, Tuesday 01 June 2021 07:16:35 PM IST

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,4 @@
-const tilt = $('.js-tilt').tilt();
-tilt.on('change', callback);  // parameters: event, transforms
-tilt.on('tilt.mouseLeave', callback); // parameters: event
-tilt.on('tilt.mouseEnter', callback);
+$('.js-tilt').tilt({
+    glare: true,
+    maxGlare: .5
+})
