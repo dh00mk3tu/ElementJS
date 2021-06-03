829bd0e1050f3348c6223b9c3dec0b7cfe2236ac
dh00mk3tu <cyberhybird@hotmail.com>
2021-06-03 16:22:19 +0530

    This commit is made by Auto-Git 13, Thursday 03 June 2021 04:08:55 PM IST

--- Pages/ElementJS-Core/episode11/orbit.js
+++ Pages/ElementJS-Core/episode11/orbit.js
@@ -6,7 +6,7 @@ window.onload = function() {
 		sun = particle.create(width / 2, height / 2, 0, 0);
 		planet = particle.create(width / 2 + 200, height / 2, 10, -Math.PI / 2);
 
-	sun.mass = 20000;
+	sun.mass = 20;
 
 	update();
 
@@ -15,13 +15,13 @@ window.onload = function() {
 		context.clearRect(0, 0, width, height);
 
 		// animation goes here
-		
+
 		planet.gravitateTo(sun);
 		planet.update();
 
 		context.beginPath();
 		context.fillStyle = "#ffff00";
-		context.arc(sun.position.getX(), sun.position.getY(), 20, 0, Math.PI * 2, false);
+		context.arc(sun.position.getX(), sun.position.getY(), 10, 0, Math.PI * 2, false);
 		context.fill();
 
 		context.beginPath();
