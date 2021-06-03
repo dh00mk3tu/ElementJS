61d71c5a1ed52cf33e636113fdbaa70b1ffabcd2
dh00mk3tu <cyberhybird@hotmail.com>
2021-06-03 16:24:37 +0530

    This commit is made by Auto-Git 15, Thursday 03 June 2021 04:08:55 PM IST

--- Pages/ElementJS-Core/episode11/main.js
+++ Pages/ElementJS-Core/episode11/main.js
@@ -5,6 +5,7 @@ window.onload = function() {
 		height = canvas.height = window.innerHeight,
 		sun = particle.create(width / 2, height / 2, 0, 0),
 		planet = particle.create(width / 2 + 200, height / 2, 10, -Math.PI / 2);
+		planet2 = particle.create(width / 2 + 250, height / 2, 10, -Math.PI / 2);
 
 	sun.mass = 20000;
 
@@ -15,7 +16,10 @@ window.onload = function() {
 		context.clearRect(0, 0, width, height);
 
 		planet.gravitateTo(sun);
+		planet2.gravitateTo(sun);
 		planet.update();
+
+		planet2.update();
 
 		context.beginPath();
 		context.fillStyle = "#ffff00";
@@ -27,6 +31,10 @@ window.onload = function() {
 		context.arc(planet.position.getX(), planet.position.getY(), 5, 0, Math.PI * 2, false);
 		context.fill();
 
+		context.beginPath();
+		context.fillStyle = "#0a03ff";
+		context.arc(planet2.position.getX(), planet2.position.getY(), 5, 0, Math.PI * 2, false);
+		context.fill();
 		// animation goes here
 
 		requestAnimationFrame(update);
